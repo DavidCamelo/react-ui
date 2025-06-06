3186b28c75da40ca250d773ee84e0b712ad7f601
David Camelo <dacaro16@gmail.com>
2025-06-06 18:16:51 -0500

    Setup React UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Header } from 'components_ui/Header'
 import User from 'user_ui/User'
 import Product from 'product_ui/Product'
 
@@ -10,28 +8,9 @@ function App() {
 
   return (
     <>
+      <Header label="Example Header" onCreateAccount={() => {}} onLogin={() => {}} onLogout={() => {}} user={{name: 'David Camelo'}} />
       <User />
       <Product />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
