23c4452a32d3774ac53dcf9623ede2cf45ab6cdc
David Camelo <dacaro16@gmail.com>
2025-06-12 20:34:34 -0500

    Setup React UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
-import { Tabs } from 'components_ui/Tabs';
-import { UsersPage } from 'user_ui/UsersPage';
-import { ProductsPage } from 'product_ui/ProductsPage';
+import React from 'react';
 import { Header } from 'components_ui/Header';
+import { TabsLayout } from './tabs-layout/TabsLayout';
 import './App.css';
 
 const App = () => {
-  const [activeTab, setActiveTab] = useState('users');
-  const tabs = [
-      { name: 'users', label: 'Users' },
-      { name: 'products', label: 'Products' },
-  ];
-
   return (
     <>
       <Header label="Example Header" onCreateAccount={() => {}} onLogin={() => {}} onLogout={() => {}} user={{name: 'David Camelo'}}/>
-      <div className="container mx-auto p-4 bg-gray-50 min-h-screen">
-        <Tabs tabs={tabs} activeTab={activeTab} onTabClick={setActiveTab} />
-
-        <div className="mt-6">
-          {activeTab === 'users' && <UsersPage />}
-          {activeTab === 'products' && <ProductsPage />}
-        </div>
-      </div>
+      <TabsLayout />
     </>
   );
 };
---
+++ src/tabs-layout/TabsLayout.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Tabs } from 'components_ui/Tabs';
+import { UsersPage } from 'user_ui/UsersPage';
+import { ProductsPage } from 'product_ui/ProductsPage';
+
+export const TabsLayout = () => {
+  const [activeTab, setActiveTab] = useState('users');
+
+  const tabs = [
+      { name: 'users', label: 'Users' },
+      { name: 'products', label: 'Products' },
+  ];
+
+  return (
+    <>
+      <div className="container mx-auto p-4 bg-gray-50 min-h-screen">
+        <Tabs tabs={tabs} activeTab={activeTab} onTabClick={setActiveTab} />
+
+        <div className="mt-6">
+          {activeTab === 'users' && <UsersPage />}
+          {activeTab === 'products' && <ProductsPage />}
+        </div>
+      </div>
+    </>
+  );
+};
